a3f76e48df6b6c75c1a0787bc88e8ed6a749305b
Saad-naeem0 <100247988+Saad-naeem0@users.noreply.github.com>
2024-01-28 04:24:44 +0500

    fixed default day color

--- lib/extra/color.dart
+++ lib/extra/color.dart
@@ -10,7 +10,7 @@ class AppColors {
   AppColors._();
 
   static const Color defaultDateColor = Colors.black;
-  static const Color defaultDayColor = Colors.black;
+  static const Color defaultDayColor = Colors.white;
   static const Color defaultMonthColor = Colors.black;
   static const Color defaultSelectionColor = Color(0x30000000);
   static const Color defaultDeactivatedColor = Color(0xFF666666);
